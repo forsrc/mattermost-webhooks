a209646388897b62cb7426cdf2f0442e4be55981
yes <forsrc@gmail.com>
2018-01-12 14:24:08 +0900

    Create test.js

---
+++ src/main/resources/js/test.js
@@ -0,0 +1,10 @@
+
+
+function toText(queryString, payloadString) {
+    var text = "\n## test ##\n -> " + new Date() + "\n";
+    //var query = JSON.parse(query);
+    //var payload = JSON.parse(payloadString);
+
+
+    return text;
+}
