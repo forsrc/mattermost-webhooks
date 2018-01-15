8dc6da71fce88f985091276a678b90d3d3afd368
yes <forsrc@gmail.com>
2018-01-15 13:53:17 +0900

    Update gitbucket.js

--- src/main/resources/js/gitbucket.js
+++ src/main/resources/js/gitbucket.js
@@ -9,38 +9,66 @@ function toText(queryString, payloadString) {
 
     var pr = payload.pull_request;
 
+    var project = payload.project;
+    if (project) {
+        text += "\n#### " + "[project](" + project.url + ") #### \n";
+        text += "\n";
+
+        text += "| name                | text                                   | other |" + "\n";
+        text += "|:--------------------|:---------------------------------------|:------|" + "\n";
+        text += "| project             | " + project.name                   + " | -     |" + "\n";
+        text += "| status              | " + payload.action                 + " | -     |" + "\n";
+        text += "| creator             | " + "@" + project.creator.login    + " | -     |" + "\n";
+        text += "| url                 | " + project.url                    + " | -     |" + "\n";
+        text += "| repository          | " + payload.repository.full_name   + " | -     |" + "\n";
+        text += "** message: ** \n";
+        text += "- [x] " + project.body;
+    }
+
+    var issue = payload.issue;
+    if (issue) {
+        text += "\n#### " + "[issue](" + issue.html_url + ") #### \n";
+        text += "\n";
+
+        text += "| name                | text                                   | other |" + "\n";
+        text += "|:--------------------|:---------------------------------------|:------|" + "\n";
+        text += "| title               | " + issue.title                    + " | -     |" + "\n";
+        text += "| user                | " + "@" + issue.user.login         + " | -     |" + "\n";
+        text += "| url                 | " + issue.html_url                 + " | -     |" + "\n";
+        text += "| repository          | " + payload.repository.full_name   + " | -     |" + "\n";
+        text += "** message: ** \n";
+        text += "- [x] " + issue.body;
+    }
 
     var review = payload.review;
     if (review) {
-        text += "\n#### " + "[PR review](" + review.html_url + ") #### \n";
+        text += "\n#### " + "[Review](" + review.html_url + ") #### \n";
         text += "\n";
 
         text += "| name                | text                                  | other |" + "\n";
         text += "|:--------------------|:--------------------------------------|:------|" + "\n";
         text += "| user                | " +  "@" + review.user.login      + " | -     |" + "\n";
         text += "| url                 | " + review.html_url               + " | -     |" + "\n";
-        //text += "| body                | " + review.body                   + " | -     |" + "\n";
-        text += "| url                 | " + review.html_url               + " | -     |" + "\n";
         text += "| repository          | " + payload.repository.full_name  + " | -     |" + "\n";
         text += "** message: ** \n";
-        text += "- [x] " + payload.body;
+        text += "- [x] " + review.body;
+        return text;
 
     }
 
     var comment = payload.comment;
     if (comment) {
-        text += "\n#### " + "[PR comment](" + comment.html_url + ") #### \n";
+        text += "\n#### " + "[Comment](" + comment.html_url + ") #### \n";
         text += "\n";
 
         text += "| name                | text                                   | other |" + "\n";
         text += "|:--------------------|:---------------------------------------|:------|" + "\n";
         text += "| user                | " + "@" + comment.user.login       + " | -     |" + "\n";
         text += "| url                 | " + comment.html_url               + " | -     |" + "\n";
-        //text += "| body                | " + comment.body                   + " | -     |" + "\n";
-        text += "| url                 | " + comment.html_url               + " | -     |" + "\n";
         text += "| repository          | " + payload.repository.full_name   + " | -     |" + "\n";
         text += "** message: ** \n";
-        text += "- [x] " + payload.body;
+        text += "- [x] " + comment.body;
+        return text;
     }
 
     var pusher = payload.pusher;
@@ -70,9 +98,9 @@ function toText(queryString, payloadString) {
 
         text += "| name                | text                                  | other |" + "\n";
         text += "|:--------------------|:--------------------------------------|:------|" + "\n";
+        text += "| title               | " + pr.title                      + " | -     |" + "\n";
         text += "| status              | " + payload.action                + " | -     |" + "\n";
         text += "| number              | " + pr.number                     + " | -     |" + "\n";
-        text += "| title               | " + pr.title                      + " | -     |" + "\n";
         text += "| url                 | " + pr.html_url                   + " | -     |" + "\n";
         text += "| created_at          | " + pr.created_at                 + " | -     |" + "\n";
         text += "| updated_at          | " + pr.updated_at                 + " | -     |" + "\n";
